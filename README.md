994734555e4f703e2110fa2f5887a1bb75e99259
rahil-p <rmp2177@gmail.com>
2021-05-21 03:14:24 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# passport-discord-token
+
+Passport strategy for authorizing users with Discord access tokens using the OAuth 2.0 API.
+
+This module lets you authenticate using Discord in your Node.js applications. By plugging into Passport, Discord 
+authentication can be easily and unobtrusively integrated into any application or framework that supports Connect-style
+middleware, including Express.
+
+####Note:
+
+_This strategy is primarily intended for authorizing requests from native clients that must obtain a Discord 
+access token using client-side flows to authenticate with your Node.js backend. For browser clients,
+a strategy like [passport-discord](https://github.com/nicholastay/passport-discord.git) is recommended._
+
+![Build Status](https://img.shields.io/travis/drudge/passport-discord-token.svg)
+![Coverage](https://img.shields.io/coveralls/drudge/passport-discord-token.svg)
+
+## Installation
+```shell
+npm install passport-discord-token
+```
+or
+```shell
+yarn add passport-discord-token
+```
+
+## Usage
+
+### Server-Side
+
+#### Create an Application
+Before using `passport-discord-token`, you must register an application with Discord.  If you have not already done so,
+create an application in the [Discord Developer Portal](https://discord.com/developers/applications).  Your application
+will be issued a client ID and client secret, which need to be provided to the strategy.
+
+#### Configure Strategy
+
+The strategy requires a `verify` callback, which accepts these credentials and calls `done` providing a `user`, as well
+as options specifying a client ID and client secret.
+
+```js
+const DiscordTokenStrategy = require('passport-discord-token');
+
+passport.use(new DiscordTokenStrategy({
+    clientID: DISCORD_CLIENT_ID,
+    clientSecret: DISCORD_CLIENT_SECRET,
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOrCreate({discordId: profile.id}, (error, user) => {
+        return done(error, user);
+    });
+}));
+```
+
+#### Authenticate Requests
+
+Use `passport.authenticate()`, specifying the 'discord-token' strategy, to authenticate requests.
+
+For example, as route middleware in an Express application:
+
+```js
+app.get('/auth/discord-token', passport.authenticate('discord-token'));
+```
+
+### Client-Side
+
+#### Configure Client
+Your client must first obtain an access token from Discord's OAuth 2.0 endpoints before making a request to this
+strategy ([_see note above_](#note)). Here are some options:
+
+- [**Discord Game SDK**](https://discord.com/developers/docs/game-sdk/sdk-starter-guide) - Call
+  [`ApplicationManager.GetOAuth2Token`](https://discord.com/developers/docs/game-sdk/applications#getoauth2token) to
+  obtain a token directly from your game.
+- **Authorization Code with PKCE Flow*** - A secure means for obtaining an access token from a native client. _Although
+  this flow is supported by Discord's API, documentation is pending. See the
+  [RFC7636](https://datatracker.ietf.org/doc/html/rfc7636) specification for guidance._
+- **[Implicit Grant](https://discord.com/developers/docs/topics/oauth2#implicit-grant)**
+
+#### Client Requests
+
+
+
+## License
+[MIT License](https://github.com/rahil-p/passport-discord-token/blob/master/LICENSE)
---
+++ src/index.js
@@ -0,0 +1,124 @@
+const {OAuth2Strategy, InternalOAuthError} = require('passport-oauth');
+
+/**
+ * `DiscordTokenStrategy` for Passport.js.
+ *
+ * This authentication strategy delegates a provided Discord access token to retrieve a user profile from Discord using
+ * the OAuth 2.0 protocol.
+ *
+ * This strategy is primarily intended for authorizing requests from native clients that have obtained a Discord access
+ * token using the Discord Game SDK or the OAuth 2.0 Authorization Code with PKCE flow.
+ *
+ * @type {DiscordTokenStrategy}
+ * @extends OAuth2Strategy
+ */
+class DiscordTokenStrategy extends OAuth2Strategy {
+	/**
+	 * Constructs an instance of the `DiscordTokenStrategy`
+	 * @param options
+	 * @param verify
+	 */
+	constructor(options = {
+		authorizationURL: 'https://discord.com/api/oauth2/authorize',
+		tokenURL: 'https://discord.com/api/oauth2/token',
+		accessTokenField: 'access_token',
+		refreshTokenField: 'refresh_token',
+		profileURL: 'https://discord.com/api/users/@me',
+
+	}, verify) {
+		super(options, verify);
+
+		this.name = 'discord-token';
+		this._accessTokenField = options.accessTokenField;
+		this._refreshTokenField = options.refreshTokenField;
+		this._profileURL = options.profileURL;
+		this._oauth2.useAuthorizationHeaderforGET(true);
+	}
+
+	/**
+	 * Authenticates a request by delegating its provided access token to Discord to retrieve the user profile
+	 * @param {Object} req = HTTP request object
+	 */
+	authenticate(req) {
+		const accessToken = this.constructor.lookup(req, this._accessTokenField)
+			|| this.constructor.parseOAuth2Header(req);
+		const refreshToken = this.constructor.lookup(req, this._refreshTokenField);
+
+		if (!accessToken) {
+			return this.fail({
+				message: `Access token not found in the ${this._accessTokenField} field`,
+			});
+		}
+
+		this._loadUserProfile(accessToken, (error, profile) => {
+			if (error) return this.error(error);
+
+			this._verify(...[
+				...(this._passReqToCallback ? [req] : []),
+				accessToken,
+				refreshToken,
+				profile,
+				(err, user, info) => {
+					if (err) return this.error(err);
+					if (!user) return this.fail(info);
+
+					return this.success(user, info);
+				},
+			]);
+		});
+	}
+
+	/**
+	 * Retrieve the Discord user profile
+	 * @param {String} accessToken
+	 * @param {Function} done
+	 */
+	userProfile(accessToken, done) {
+		this._oauth2.get(this._profileURL, accessToken, (error, body) => {
+			if (error) return done(new InternalOAuthError('Failed to fetch user profile', error));
+
+			try {
+				const json = JSON.parse(body);
+
+				const profile = {
+					provider: 'discord',
+					...json,
+				};
+
+				done(null, profile);
+			} catch (err) {
+				done(err);
+			}
+		});
+	}
+
+	/**
+	 * Looks up the value of a field within a request; this method checks in order the body, query, and headers of a
+	 * request until the value is found
+	 * @param {Object} req = HTTP request object
+	 * @param {String} field - lookup field
+	 * @return {false | String} lookup value for field within the body, query, or headers of the request
+	 */
+	static lookup(req, field) {
+		return (req.body && req.body[field])
+			|| (req.query && req.query[field])
+			|| (req.headers && (req.headers[field] || req.headers[field.toLowerCase()]));
+	}
+
+	/**
+	 * Parses an OAuth2 RFC6750 bearer authorization token from the headers of a request
+	 * @param {Object} req = HTTP request object
+	 * @return {false | String} bearer authorization token
+	 */
+	static parseOAuth2Header(req) {
+		const headerValue = req.headers && (req.headers.Authorization || req.headers.authorization);
+
+		return headerValue && (() => {
+			const bearerRgx = /Bearer (.*)/;
+			const match = headerValue.match(bearerRgx);
+			return match && match[1];
+		});
+	}
+}
+
+module.exports = DiscordTokenStrategy;
